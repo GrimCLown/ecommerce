d61c3d14ce2eb451cb9108a4314b7adc3082329e
GrimCLown <ranack21@gmail.com>
2024-02-01 20:36:54 +0800

    Update HeroSection.jsx

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./HeroSection.css";
-import config from "../../config.json";
 
 const HeroSection = ({ title, subtitle, link, image }) => {
   return (
@@ -8,7 +7,7 @@ const HeroSection = ({ title, subtitle, link, image }) => {
       <div className="align_center">
         <h2 className="hero_title">{title}</h2>
         <p className="hero_subtitle">{subtitle}</p>
-        <a href={`${config.backendURL}` + link} className="hero_link">
+        <a href={link} className="hero_link">
           Buy Now
         </a>
       </div>

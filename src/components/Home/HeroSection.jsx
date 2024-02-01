93db521d832993e5a9c0042040f6075341ef9ee3
GrimCLown <ranack21@gmail.com>
2024-02-01 20:05:32 +0800

    Update HeroSection.jsx

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = ({ title, subtitle, link, image }) => {
       <div className="align_center">
         <h2 className="hero_title">{title}</h2>
         <p className="hero_subtitle">{subtitle}</p>
-        <a href={link} className="hero_link">
+        <a href={`${config.backendURL}+link`} className="hero_link">
           Buy Now
         </a>
       </div>
